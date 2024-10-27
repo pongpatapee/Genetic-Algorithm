24413f6ee0cad1b1c862cb405e15d620f972c6bd
pongpatapee <pongpatapee1021@gmail.com>
2024-10-27 03:01:50 -0400

    feat: initial genetic algorithm working but seems slow

---
+++ evolutionary_computing/shakespeare-monkey/dna.js
@@ -0,0 +1,69 @@
+function randomCharacter() {
+  // Ascii numbers for characters
+  let c = floor(random(32, 127));
+  return String.fromCharCode(c);
+}
+
+class DNA {
+  constructor(length) {
+    this.genes = [];
+    this.fitness = 0;
+
+    for (let i = 0; i < length; i++) {
+      this.genes[i] = randomCharacter();
+    }
+  }
+
+  getPhrase() {
+    return this.genes.join("");
+  }
+
+  // calculates percentage of correct characters
+  calcFintess(target) {
+    let score = 0;
+    for (let i = 0; i < this.genes.length; i++) {
+      if (this.genes[i] === target[i]) {
+        score++;
+      }
+    }
+
+    this.fitness = score / target.length;
+  }
+
+  crossOverMidPointStrategy(partner) {
+    let child = new DNA(this.genes.length);
+    let midpoint = random(this.genes.length);
+    for (let i = 0; i < this.genes.length; i++) {
+      if (i < midpoint) {
+        child.genes[i] = this.genes[i];
+      } else {
+        child.genes[i] = partner.genes[i];
+      }
+    }
+  }
+
+  crossOverCoinFlip(partner) {
+    let child = new DNA(this.genes.length);
+    for (let i = 0; i < this.genes.length; i++) {
+      if (random() < 0.5) {
+        child.genes[i] = this.genes[i];
+      } else {
+        child.genes[i] = partner.genes[i];
+      }
+    }
+
+    return child;
+  }
+
+  crossOver(partner) {
+    return this.crossOverCoinFlip(partner);
+  }
+
+  mutate(mutationRate) {
+    for (let i = 0; i < this.genes.length; i++) {
+      if (random() < mutationRate) {
+        this.genes[i] = randomCharacter();
+      }
+    }
+  }
+}
--- evolutionary_computing/shakespeare-monkey/sketch.js
+++ evolutionary_computing/shakespeare-monkey/sketch.js
@@ -1,7 +1,75 @@
+let mutationRate = 0.01;
+let populationSize = 150;
+let population = [];
+let target = "this is the target phrase";
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(640, 900);
+
+  for (let i = 0; i < populationSize; i++) {
+    population[i] = new DNA(target.length);
+  }
 }
 
 function draw() {
-  background(220);
+  background(255);
+
+  for (let phrase of population) {
+    phrase.calcFintess(target);
+  }
+
+  fill(0);
+  textFont("Courier");
+  textSize(12);
+  text(getAllPhrase(), width - 300, 30);
+
+  let best = getBestPhrase();
+  text(best.getPhrase(), 10, 30);
+
+  // mating pool will help with
+  // the probability distribution for
+  // picking parents based on their fitness
+  let matingPool = [];
+
+  for (let phrase of population) {
+    let n = floor(phrase.fitness * 100);
+    for (let j = 0; j < n; j++) {
+      matingPool.push(phrase);
+    }
+  }
+
+  for (let i = 0; i < population.length; i++) {
+    let parentA = random(matingPool);
+    let parentB = random(matingPool);
+
+    let child = parentA.crossOver(parentB);
+
+    child.mutate(mutationRate);
+
+    population[i] = child;
+  }
+}
+
+function getBestPhrase() {
+  let maxFitness = 0;
+  let best = population[0];
+
+  for (let phrase of population) {
+    if (phrase.fitness > maxFitness) {
+      maxFitness = phrase.fitness;
+      best = phrase;
+    }
+  }
+  return best;
+}
+
+function getAllPhrase() {
+  let phrases = "";
+
+  for (let i = 0; i < population.length; i++) {
+    phrases += population[i].getPhrase();
+    phrases += "\n";
+  }
+
+  return phrases;
 }
